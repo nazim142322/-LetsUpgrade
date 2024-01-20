a9f8be09a181e55275dad423511dd6fd6b069a8e
nazim142322 <nazim142322@gmail.com>
2024-01-20 22:36:01 +0530

    react components

---
+++ myfirstapp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <h1>this is header</h1>
+    </div>
+  );
+}
+export default Header;
---
+++ myfirstapp/src/components/Product.js
@@ -0,0 +1,10 @@
+import "./Product.css";
+function Product() {
+  return (
+    <div className="product">
+      <h3>samsung</h3>
+      <p>Rs 20000</p>
+    </div>
+  );
+}
+export default Product;
