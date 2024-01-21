21dd7014ac8e3aaa9e9e11ca1658749b04434242
nazim142322 <nazim142322@gmail.com>
2024-01-21 20:36:51 +0530

    notes added

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 function App() {
-  //let login;//false
-  //let login = "nazim";//true
-  //let login = 0; //false
-  let login = 1; //true
+  let login = 0;
 
-  return <div className="App">{login ? <h1>true</h1> : <h1>false</h1>}</div>;
+  return (
+    <div className="App">
+      {(() => {
+        if (login) {
+          return (
+            <div>
+              <h1>true</h1>
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <h1>False</h1>
+            </div>
+          );
+        }
+      })()}
+    </div>
+  );
 }
 
 export default App;
