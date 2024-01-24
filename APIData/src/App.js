77cfb59f28e66628d8c23cf454a70caeb1d30209
Nazim <nazim142322@gmail.com>
2024-01-24 12:42:15 +0530

    react project added

--- APIData/src/App.js
+++ APIData/src/App.js
@@ -1,27 +1,42 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState  } from "react";
 function App() {
   const [users, setUser] = useState([]);
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((res) => res.json())
-      .then((data) => {
+
+
+    async function fetchData () {
+      try{
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        if(!res.ok){
+          throw Error(res.statusText)
+        }
+        const data = await res.json();
         console.log(data);
-        setUser(data);
-      });
+        console.log(typeof data)//object
+        setUser(data); 
+      }
+      catch(error){
+       alert(error)
+      }
+    }
+   fetchData();
   }, []);
   return (
     <div className="App">
       <h1>hello world</h1>
-      {/* <pre>{JSON.stringify(users)}</pre> */}
-      {users.map((elm, index) => {
-        return (
-          <div key={index}>
-            <h3>Name - {elm.name}</h3>
-            <h3>email - {elm.email}</h3>
-          </div>
-        );
-      })}
+      <pre>{JSON.stringify(users)}</pre>
+      {
+        users.map((elm, key)=>{
+          return (
+            <div key={key}>
+                <h1>{elm.name}</h1>
+                <h2>{elm.email}</h2>
+            </div>
+          )
+          
+        })
+      }      
     </div>
   );
 }
