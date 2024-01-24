4c957c644f2be954c20ffc3bdb46a9aae233445c
nazim142322 <nazim142322@gmail.com>
2024-01-24 21:54:34 +0530

    code added

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import News from "./News";
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=india&from=2024-01-23&apiKey=5e4f58abc8d04d3abc98b34ab1629283`
+    )
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        console.log(data.articles);
+        setNews(data.articles);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>News</h1>
+        <input type="text" placeholder="Search News" />
       </header>
+
+      <section className="news-articles">
+        <News />
+        <News />
+        <News />
+        <News />
+      </section>
     </div>
   );
 }
---
+++ myproject/src/News.js
@@ -0,0 +1,8 @@
+function News() {
+  return (
+    <div className="news">
+      <h2>news</h2>
+    </div>
+  );
+}
+export default News;
