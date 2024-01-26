e8ff3f4962f03eab621b3b32d442cde6c8380fff
nazim142322 <nazim142322@gmail.com>
2024-01-26 21:42:01 +0530

    project added

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -4,32 +4,63 @@ import News from "./News";
 
 function App() {
   const [news, setNews] = useState([]);
+  const [category, setCategory] = useState("india");
+  const key = "5e4f58abc8d04d3abc98b34ab1629283";
   useEffect(() => {
     fetch(
-      `https://newsapi.org/v2/everything?q=india&from=2024-01-23&apiKey=5e4f58abc8d04d3abc98b34ab1629283`
+      `https://newsapi.org/v2/everything?q=${category}&from=2024-01-23&apiKey=${key}`
     )
       .then((res) => {
-        console.log(res);
+        // console.log(res);
         return res.json();
       })
       .then((data) => {
-        console.log(data);
-        console.log(data.articles);
-        setNews(data.articles);
+        // console.log(data);
+        // console.log(data.articles);
+        if (data.message !== "") {
+          setNews(data.articles);
+        } else {
+          alert("key expire");
+        }
       });
-  }, []);
+  }, [category]);
   return (
     <div className="App">
       <header>
         <h1>News</h1>
-        <input type="text" placeholder="Search News" />
+        <input
+          type="text"
+          onChange={(event) => {
+            if (event.target.value !== "") {
+              setCategory(event.target.value);
+            } else {
+              setCategory("india");
+            }
+          }}
+          placeholder="Search News by category"
+        />
       </header>
 
       <section className="news-articles">
-        <News />
-        <News />
-        <News />
-        <News />
+        {news.length !== 0 ? (
+          news
+            .filter((article) => article.title !== "[Removed]")
+            .map((article, key) => {
+              return (
+                <News
+                  key={key}
+                  img={article.urlToImage}
+                  title={article.title}
+                  news={article.content}
+                  link={article.url}
+                  author={article.author}
+                  source={article.source.name}
+                />
+              );
+            })
+        ) : (
+          <h1>News not found</h1>
+        )}
       </section>
     </div>
   );
--- myproject/src/News.js
+++ myproject/src/News.js
@@ -1,7 +1,23 @@
-function News() {
+function News(props) {
+  // console.log(props.source);
   return (
     <div className="news">
-      <h2>news</h2>
+      <div className="newsImg">
+        {props.img !== null ? (
+          <img src={props.img} alt="" />
+        ) : (
+          <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg" />
+        )}
+      </div>
+      <h2>{props.title}</h2>
+      <p>{props.news?.substring(0, 100).concat("...")}</p>
+      <a href={props.link} target="_blank">
+        Read More
+      </a>
+      <div className="source">
+        <p>Author : {props.author}</p>
+        <p>{props.source}</p>
+      </div>
     </div>
   );
 }
